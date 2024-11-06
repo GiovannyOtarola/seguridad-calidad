a932c89a58eb97b1531de151f16703bca10224ff
Giovanny <giovannyxdd@gmail.com>
2024-11-06 20:13:25 -0300

    Update CustomAuthenticationProvider.java

--- src/main/java/com/duoc/seguridad_calidad/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/duoc/seguridad_calidad/provider/CustomAuthenticationProvider.java
@@ -71,9 +71,15 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
             return new UsernamePasswordAuthenticationToken(name, password, authorities);
 
         } catch (Exception ex) {
-            logger.error("Error during authentication for user '{}'. Cause: {}, Error Message: {}", 
-                         authentication.getName(), ex.getClass().getSimpleName(), ex.getMessage(), ex);
-            throw new BadCredentialsException("Invalid username or password for user: " + authentication.getName(), ex);
+            logger.error("Error during authentication for user '{}'. Exception: {}, Cause: {}, Message: {}", 
+            authentication.getName(), 
+            ex.getClass().getSimpleName(), 
+            ex.getCause(), 
+            ex.getMessage(), 
+            ex);
+
+            throw new BadCredentialsException(
+            "Invalid username or password for user: " + authentication.getName(), ex);
         }
     }
 
